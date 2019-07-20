2e14ea97cd0a600bfded6ca8fe423af590d3803b
killsyre <killsyre@yahoo.com>
2019-07-20 12:14:25 +0100

    Revised code for filters

--- public/js/main.js
+++ public/js/main.js
@@ -25,11 +25,6 @@ $(document).ready(function () {
     var fieldsInput = $('.fields input');
     var minPrice = $('#min_price');
     var maxPrice = $('#max_price');
-    //Focus event
-    fieldsInput.on('focus', function (e) {
-        // var parent = $(e.target).parents('.sorting__list:eq(0)');
-        // $(parent).find('span.start').text($(this).data('role'));
-    });
 
     //Keyup event
     const price = {
@@ -42,103 +37,118 @@ $(document).ready(function () {
             flag: 'inférieur à : '
         }
     };
-    minPrice.on('keyup', function (e) {
-        var priceValue = $(this).val();
-        var dashPrice = $('#dash_price');
-        price.min.value = priceValue;
-        if ($(this).siblings('#max_price').val().length === 0) {
-            var flag = price.min.flag;
-            if (priceValue.length === 0){
-                flag = '';
-            }
-            priceValue = flag  + priceValue;
-        } else {
-            if (!dashPrice.visible) {
-                dashPrice.css({
-                    visibility: 'visible'
-                });
-            }
 
-            if (priceValue.length > 0) {
-                var txt = $('#end_price').text();
-                var replaced = txt.replace(price.max.flag, '');
-                $('#end_price').text(replaced);
-                //console.log(price.min.flag)
-            } else {
-                var txt = $('#end_price').text();
-                var replaced = price.max.flag + txt;
-                $('#end_price').text(replaced);
-            }
-        }
-        $('#start_price').text(priceValue + ' DH ');
-        if ($(this).val().length === 0) {
-            dashPrice.css({
-                visibility: 'hidden'
-            });
-            $('#start_price').text('');
-        }
-
-    });
-
-    maxPrice.on('keyup', function (e) {
-        var priceValue = $(this).val();
-        var dashPrice = $('#dash_price');
-        price.max.value = priceValue;
-        if ($(this).siblings('#min_price').val().length === 0) {
-            var flag = price.max.flag;
-            if (priceValue.length === 0){
-                flag = '';
-            }
-            priceValue = flag  + priceValue;
-        } else {
-
-            if (!dashPrice.visible) {
-                dashPrice.css({
-                    visibility: 'visible'
-                });
-            }
-
-            if (priceValue.length > 0) {
-                var txt = $('#start_price').text();
-                var replaced = txt.replace(price.min.flag, '');
-                $('#start_price').text(replaced);
-                //console.log(price.min.flag)
-            } else {
-                var txt = $('#start_price').text();
-                var replaced = price.min.flag + txt;
-                $('#start_price').text(replaced);
-            }
-
-        }
-        $('#end_price').text(priceValue + ' DH ');
-        if ($(this).val().length === 0) {
-            dashPrice.css({
-                visibility: 'hidden'
-            });
-            $('#end_price').text('');
-        }
-    });
-
-    //Change calue event
+    //Change value event
     fieldsInput.on('keyup', function (e) {
-        /*if (isNaN(parseInt(e.key)) || e.target.value.length > 4) {
-            str = e.target.value;
-            sliced = str.slice(0, -1);
-            e.target.value = sliced;
-            return false;
-        }
-        var parent = $(e.target).parents('.sorting__list:eq(0)');
-        if ($(this).siblings('input').val() === '') {
-            $(parent).find('span.start').text($(this).data('role') + $(this).val() + ' DH');
-            $(parent).find('span.end').text('');
-        }else {
-            if ($(this).hasClass('mx')) {
-                $(parent).find('span.end').text($(this).val() + ' DH ');
-            }else{
-                $(parent).find('span.start').text($(this).val() + ' DH - ');
+
+        if (!isNaN(parseInt(e.key)) || e.key === 'Backspace' ) {
+
+            var currentValue = $(this).val();
+            var startPrice = $(this).parents('.fields:eq(0)').prev().find('span.result > .start');
+            var endPrice = $(this).parents('.fields:eq(0)').prev().find('span.result > .end');
+            var dash = $(this).parents('.fields:eq(0)').prev().find('span.result > .dash');
+            var unit;
+            var flag = '';
+            var spanTarget;
+
+            if (e.target.dataset.unit === 'DH') {
+                unit = ' DH';
+            } else {
+                unit = ' M²';
             }
 
-        }*/
+            switch (e.target.className) {
+                case 'mn':
+                    flag = price.min.flag;
+                    spanTarget = startPrice;
+                    break;
+                case 'mx':
+                    flag = price.max.flag;
+                    spanTarget = endPrice;
+                    break;
+            }
+
+            if ($(this).siblings('input').val().length === 0) {
+
+                if (currentValue.length === 0) {
+                    flag = '';
+                    unit = '';
+                }
+                currentValue = flag + currentValue;
+                spanTarget.text(currentValue + unit);
+
+            } else {
+
+                var siblingText = spanTarget.siblings('span').not('.dash');
+
+                if (!dash.visible) {
+                    dash.css({
+                        visibility: 'visible'
+                    })
+                }
+
+                if ($(this).val().length === 0) {
+
+                    dash.css({
+                        visibility: 'hidden'
+                    });
+                    unit = '';
+
+                    if (e.target.className === 'mn') {
+                        flag = price.max.flag
+                    } else {
+                        flag = price.min.flag;
+                    }
+                    siblingText.html( flag + siblingText.html() + unit);
+                } else {
+                    siblingText.html(siblingText.html().replace(/[^0-9]/g, '') + unit);
+                }
+
+                spanTarget.text(currentValue + unit);
+            }
+        }
+
+    })
+    
+    //Select pieces
+    var checkRooms = $('.checkbox input[type=checkbox]');
+
+    checkRooms.on('click', function (e) {
+
+        var currentValue = $(this).val();
+        var startPrice = $(this).parents('.pieces:eq(0)').prev().find('span.result > .start');
+        var endPrice = $(this).parents('.pieces:eq(0)').prev().find('span.result > .end');
+        var dash = $(this).parents('.pieces:eq(0)').prev().find('span.result > .dash');
+        var checked = $('.checkbox input:checked');
+
+
+        startPrice.text(checked.eq(0).val());
+        endPrice.text(checked.eq(1).val());
+
+
+        if (checked.length >= 2) {
+            checked.eq(1).prop('checked', false);
+            dash.css({
+                visibility: 'visible'
+            })
+        }
+
+        if (checked.length === 1) {
+            endPrice.text('');
+        }
+
+
+        if ($('.checkbox input:checked').length === 0) {
+            startPrice.text('');
+            endPrice.text('');
+            dash.css({
+                visibility: 'hidden'
+            })
+        }
+
+
+
+
 
 
     })
